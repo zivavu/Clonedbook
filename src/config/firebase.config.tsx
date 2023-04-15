e7bf58b28ea28dfe9d904052f1f2332279f58873
Tomasz Kierzenkowski <107223633+zivavu@users.noreply.github.com>
2023-04-15 15:34:57 +0200

    :wrench: Configurate firebase

---
+++ src/config/firebase.config.tsx
@@ -0,0 +1,25 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
+
+const firebaseConfig = {
+	apiKey: process.env.API_KEY,
+
+	authDomain: process.env.AUTH_DOMAIN,
+
+	databaseURL: process.env.DATABASE_URL,
+
+	projectId: process.env.PROJECT_ID,
+
+	storageBucket: process.env.STORAGE_BUCKET,
+
+	messagingSenderId: process.env.MESSENGING_SENDER_ID,
+
+	appId: process.env.APP_ID,
+};
+
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
+export const auth = getAuth(app);

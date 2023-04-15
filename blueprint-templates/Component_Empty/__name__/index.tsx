acf36fd4a4283df0ddca3be0ac704ffa19f3ed77
Tomasz Kierzenkowski <107223633+zivavu@users.noreply.github.com>
2023-04-15 18:12:03 +0200

    :wrench: Add default component template

---
+++ blueprint-templates/Component_Empty/__name__/index.tsx
@@ -0,0 +1,19 @@
+// Dependencies scoped imports
+import React from 'react';
+import { Typography } from '@mui/material';
+
+// Project scoped imports
+
+// Module scoped imports
+import { StyledRoot } from './styles';
+import { {{name}}Props } from './types';
+
+function {{name}} ( }: {{name}}Props) {
+  return (
+    <StyledRoot>
+      <Typography>{{name}}</Typography>
+    </StyledRoot>
+  );
+}
+  
+export default {{name}};

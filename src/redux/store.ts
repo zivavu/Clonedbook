3a6d0859e16bd369f998362dff2a0b533c5ddaae
Tomasz Kierzenkowski <107223633+zivavu@users.noreply.github.com>
2023-06-05 16:45:55 +0200

    :wastebasket: Delete unused endpoint

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,14 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import themeModeSlice from './features/themeSlice';
 import { loggedUser } from './services/loggedUserAPI';
-import { posts } from './services/postsAPI';
+
 import { usersPublicData } from './services/usersBasicInfoAPI';
 import { userPublicFriends } from './services/usersPublicFriendsAPI';
 
 export const store = configureStore({
   reducer: {
     [loggedUser.reducerPath]: loggedUser.reducer,
-    [posts.reducerPath]: posts.reducer,
     [usersPublicData.reducerPath]: usersPublicData.reducer,
     [userPublicFriends.reducerPath]: userPublicFriends.reducer,
     theme: themeModeSlice,
@@ -17,7 +16,6 @@ export const store = configureStore({
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({ serializableCheck: false }).concat(
       loggedUser.middleware,
-      posts.middleware,
       usersPublicData.middleware,
       userPublicFriends.middleware,
     ),

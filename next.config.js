ef625544b392c08837b2989ce1aa476fd1ae9ccc
Tomasz Kierzenkowski <107223633+zivavu@users.noreply.github.com>
2023-06-13 09:57:12 +0200

    :wrench: Update Next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  transpilePackages: ['@mui/system', '@mui/material', '@mui/icons-material'],
+  modularizeImports: {
+    '@mui/material/?(((\\w*)?/?)*)': {
+      transform: '@mui/material/{{ matches.[1] }}/{{member}}',
+    },
+    '@mui/icons-material/?(((\\w*)?/?)*)': {
+      transform: '@mui/icons-material/{{ matches.[1] }}/{{member}}',
+    },
+  },
   images: {
     remotePatterns: [
       {

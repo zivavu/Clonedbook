440519b3de4fc8d4a00d0b28d9f50109914b1577
Tomasz Kierzenkowski <zivavu@gmail.com>
2026-01-26 18:17:06 +0100

    :lipstick: Delete reaction border for comments

--- src/components/organisms/ReactionsDisplay/index.tsx
+++ src/components/organisms/ReactionsDisplay/index.tsx
@@ -74,7 +74,7 @@ export default function ReactionsDisplayBox({
                 type={reaction.type}
                 zIndex={2 - i}
                 size={size}
-                showBorder
+                showBorder={false}
               />
             );
           })}

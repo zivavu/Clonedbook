49946b6a6da5341a0bddd92f23893ad689ff1f28
Tomasz Kierzenkowski <zivavu@gmail.com>
2026-03-08 23:33:08 +0100

    Update the dependencies

--- src/components/organisms/FeedPost/PostText/index.tsx
+++ src/components/organisms/FeedPost/PostText/index.tsx
@@ -47,9 +47,8 @@ export default function PostTextArea({
       src={embedUrl}
       width='100%'
       height={152}
-      frameBorder={0}
+      border={0}
       allow='autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture'
-      loading='lazy'
       sx={{
         borderRadius: 2,
         mt: 1,

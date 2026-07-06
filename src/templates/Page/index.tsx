6be3930d12359250d7cc8270b2bcdfd880291238
Tomasz Kierzenkowski <zivavu@gmail.com>
2026-07-06 23:29:40 +0200

    Use clonagram icon as favicon

--- src/templates/Page/index.tsx
+++ src/templates/Page/index.tsx
@@ -11,6 +11,8 @@ export default function PageTemplate({ children, title, description }: PageProps
         <meta name='description' content={description || 'Miserable clone of Mark&#39;s empire.'} />
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <link rel='icon' href='/favicon.ico' />
+        <link rel='icon' type='image/png' href='/clonedbook-logo-200.png' />
+        <link rel='apple-touch-icon' href='/clonedbook-logo-200.png' />
       </Head>
       <StyledRoot>
         <main>{children}</main>
